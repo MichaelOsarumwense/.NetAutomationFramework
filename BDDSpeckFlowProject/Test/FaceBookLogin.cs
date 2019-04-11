16c19193edc57d023560f968371896741e040d31
MichaelOsarumwense <sanmicad@gmail.com>
2019-04-11 20:31:11 +0100

    Add project files.

---
+++ BDDSpeckFlowProject/Test/FaceBookLogin.cs
@@ -0,0 +1,56 @@
+﻿
+
+using FluentAssertions;
+using OpenQA.Selenium.Chrome;
+using System.Configuration;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace BDDSpeckFlowProject
+{
+
+   [Binding]
+    class FaceBookLogin
+    {
+
+        [Given(@"The User Navigates to facebook")]
+        public void GivenTheUserNavigatestoFacebook()
+        {
+            Driver.driver = new ChromeDriver();
+            Driver.driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["FoxUrl"]);
+            Driver.driver.Manage().Window.Maximize();
+            Thread.Sleep(1000);
+        }
+
+        [When(@"The user enters correct username")]
+        public void AndTheUserenterscorrectUsername()
+        {
+            LoginPage login = new LoginPage();
+            login.Email.SendKeys(ConfigurationManager.AppSettings["username"]);
+            Thread.Sleep(1000);
+        }
+
+        [When(@"The user enters correct password")]
+        public void AndTheUserenterscorrectPassword()
+        {
+            LoginPage login = new LoginPage();
+            login.Passwword.SendKeys(ConfigurationManager.AppSettings["password"]);
+            Thread.Sleep(1000);
+        }
+
+      
+        [Then(@"the user should be logged in")]
+        public void Thentheusershouldbeloggedin()
+        {
+            LoginPage login = new LoginPage();
+            login.LoginButton.Should().NotBeNull();
+           
+
+
+            Thread.Sleep(1000);
+            Driver.driver.Close();
+
+        }
+    }
+        }
+        
---
+++ BDDSpeckFlowProject/Test/MultipleLogin.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace BDDSpeckFlowProject.Test
+{
+    [Binding]
+    public class MultipleLogin
+        
+    {
+
+        [Given(@"The User Navigates tru facebook")]
+        public void GivenTheUserNavigatestruFacebook()
+        {
+            Driver.driver = new ChromeDriver();
+            Driver.driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["BaseUrl"]);
+            Driver.driver.Manage().Window.Maximize();
+            Thread.Sleep(1000);
+        }
+        [When(@"I enter (.*) and (.*)")]
+        public void WhenIEnterAnd(string username, string password)
+        {
+            LoginPage login = new LoginPage();
+            login.Email.SendKeys(username);
+            login.Passwword.SendKeys(password);
+            Thread.Sleep(3000);
+        }
+        [Then(@"the user can clear road")]
+        public void theusercanclearroad()
+        {
+            LoginPage login = new LoginPage();
+          
+            login.Email.Clear();
+            login.Passwword.Clear();
+            Driver.driver.Close();
+
+        }
+        }
+}
